6e85cbb436e613c15117c21caf749b5e3b5d3483
Arvind Sudheer <arvind.sudheer@gmail.com>
2022-11-20 11:23:32 -0500

    a8 REST fa22

---
+++ app.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import HelloController from "./controllers/hello-controller.js";
+import UserController from "./controllers/users/users-controller.js";
+import TuitsController from "./controllers/tuits/tuits-controller.js";
+import cors from 'cors';
+const app = express(); //creates an instance of the library
+app.use(cors())
+app.use(express.json())
+TuitsController(app)
+HelloController(app)
+UserController(app)
+app.listen(4000)
