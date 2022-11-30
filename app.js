dc658b617599eaddc7b91978b9a6bf2e647dfda9
Arvind Sudheer <arvind.sudheer@gmail.com>
2022-11-30 17:49:49 -0500

    a9 Mongo fa22

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ import UserController from "./controllers/users/users-controller.js";
 import TuitsController from "./controllers/tuits/tuits-controller.js";
 import cors from 'cors';
 import mongoose from "mongoose";
-mongoose.connect('mongodb+srv://arv12345:arv12345@cluster0.k2c3qd9.mongodb.net/?retryWrites=true&w=majority');
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
+    || 'mongodb://localhost:27017/tuiter-2';
+mongoose.connect(CONNECTION_STRING);
 const app = express(); //creates an instance of the library
 app.use(cors())
 app.use(express.json())
