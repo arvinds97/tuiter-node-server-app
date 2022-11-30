10ac4afa4bebe91d530e6dc4b8f67aff83ca75a4
Arvind Sudheer <arvind.sudheer@gmail.com>
2022-11-30 14:19:28 -0500

    a9 Mongo fa22

--- controllers/tuits/tuits-schema.js
+++ controllers/tuits/tuits-schema.js
@@ -3,5 +3,15 @@ const schema = mongoose.Schema({
     tuit: String,
     likes: Number,
     liked: Boolean,
+    username: String,
+    topic: String,
+    handle: String,
+    time: String,
+    image: String,
+    title: String,
+    replies: Number,
+    retuits: Number,
+    dislikes: Number,
+    disliked: Number
 }, {collection: 'tuits'});
 export default schema;
